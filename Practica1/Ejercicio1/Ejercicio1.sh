c414c50410d13aee98b4e4b87116a220835351a9
Carlos-Chauca <cchaucap@uni.pe>
2017-09-29 13:54:59 -0500

    Ejercicio 1

---
+++ Practica1/Ejercicio1/Ejercicio1.sh
@@ -0,0 +1,22 @@
+#Nombre: Carlos Renato Chauca Prinque 20140231J
+#Pasos para esta pregunta
+
+# a)
+gedit examen #(copia las siguientes 5 lineas omitiendo el "#")
+
+#Erika Rios Mendoza: 20
+#Juan Carlos Espinoza: 32
+#Claudio Lara Abila: 25
+#Carlos Chauca Prinque: 22
+#Jessica Martinez Figueroa: 28
+
+
+#b)
+gedit temp
+#copias el archivo ordenado, lo puedes obtener usando el comando "sort examen"
+
+#c)
+
+
+
+
